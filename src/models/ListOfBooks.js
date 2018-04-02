f700fe601287818b177f50e9486f7c6c4c2811ab
zhaojian <745984265@qq.com>
2018-04-02 18:38:33 +0800

    first commit

---
+++ src/models/ListOfBooks.js
@@ -0,0 +1,47 @@
+import { queryRule, removeRule, addRule,queryListOfBooks } from '../services/api';
+
+export default {
+  namespace: 'ListOfBooks',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(queryListOfBooks, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+    *add({ payload, callback }, { call, put }) {
+      const response = yield call(addRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+    *remove({ payload, callback }, { call, put }) {
+      const response = yield call(removeRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
